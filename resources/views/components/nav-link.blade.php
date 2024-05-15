b847fa6b5cfa7248136242cb9a5040e2f36c7819
DodyDharma440 <dharma.dody@yahoo.com>
2024-05-15 12:40:05 +0800

    blade component

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,4 @@
+@props(['active' => false])
 {{-- {{ $attribute }} digunakan untuk menggantikan atribute yang ada pada view (href="/") --}}
 <a {{ $attributes }}
     class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
