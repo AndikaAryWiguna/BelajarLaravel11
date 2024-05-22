0a1badbd3b6fb31dc7893db9dd3c2c50b6931135
DodyDharma440 <dharma.dody@yahoo.com>
2024-05-22 17:08:12 +0800

    post n singgle post

--- resources/views/blog.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    {{-- Component (X-slot) ini adalah Variable untuk title yang akan digunakan pada saat mengambil nilai pada route  --}}
-    {{-- Dan title ini (:slot) akan menjadi variable untuk digunakan pada view (home.blade.php atau about.blade.php, dst) --}}
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3>Ini Adalah Halaman Blog</h3>
-</x-layout>
