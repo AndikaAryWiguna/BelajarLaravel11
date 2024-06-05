169affdcde7e7bac6c803e4311632fc5fe6b0164
DodyDharma440 <dharma.dody@yahoo.com>
2024-06-05 16:04:57 +0800

    Post & Single Post Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostControler;
 use App\Models\Kategori;
 use App\Models\Post;
 use App\Models\User;
@@ -13,18 +14,10 @@ Route::get('/about', function () {
     return view('about', ['nama'=>'Made Andika Ary Wiguna','title' => 'About']);
 });
 
-Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
-});
-Route::get('/posts/{post:slug}', function(Post $post){
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
-});
-Route::get('/authors/{user:username}', function(User $user){
-    return view('posts', ['title' =>count($user->posts) . ' Artikel by : '. $user->name, 'posts' => $user->posts]);
-});
-Route::get('/kategoris/{kategori:slug}', function(Kategori $kategori){
-    return view('posts', ['title' =>'Artikel in: '. $kategori->name, 'posts' => $kategori->posts]);
-});
+Route::get('/posts', [PostControler::class, 'index']);
+Route::get('/posts/{post:slug}', [PostControler::class, 'show']);
+Route::get('/authors/{user:username}', [PostControler::class, 'author']);
+Route::get('/kategoris/{kategori:slug}', [PostControler::class, 'kategori']);
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
